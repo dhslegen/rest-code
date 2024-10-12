a34449cfb780b2b511248515a75e38a2cf926f5c
zhaowenhao <37139487+dhslegen@users.noreply.github.com>
2024-10-12 10:37:08 +0800

    Refactor electron main and preload files

--- dist-electron/preload.mjs
+++
@@ -1,7 +0,0 @@
-"use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("ipcRenderer", {
-  showSaveDialog: () => electron.ipcRenderer.invoke("showSaveDialog"),
-  showOpenDialog: () => electron.ipcRenderer.invoke("showOpenDialog"),
-  showMessageBox: (options) => electron.ipcRenderer.invoke("showMessageBox", options)
-});
